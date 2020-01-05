0722fa793b2079a1aef3045d314f848311624cc8
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2020-01-04 21:15:17 -0700

    adding dotenv for use of .env file

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line global-require
+  require('dotenv').config();
+}
+
 const { app } = require('./server');
 
 // eslint-disable-next-line no-console
